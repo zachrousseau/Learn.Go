b2b61c4c7cc9cc7e493e415f9fc3efbd6b2345d9
zachrousseau <zachrousseau0@gmail.com>
2024-08-12 21:28:46 -0400

    binary tree excercise complete with errors.

--- tour/exercise-equivalent-binary-trees.go
+++ tour/exercise-equivalent-binary-trees.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"reflect"
+	"sort"
 
 	"golang.org/x/tour/tree"
 )
@@ -21,19 +23,46 @@ func Walk(t *tree.Tree, ch chan int) {
 // Same determines whether the trees
 // t1 and t2 contain the same values.
 func Same(t1, t2 *tree.Tree) bool {
-	return false
+
+	ch1 := make(chan int, 10)
+	ch2 := make(chan int, 10)
+
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+
+	close(ch1)
+	close(ch2)
+
+	var a1 []int
+	var a2 []int
+
+	for i := range ch1 { 
+		a1 = append(a1, i)
+	}
+
+	for i := range ch2 { 
+		a2 = append(a2, i)
+	}
+
+	sort.Slice(a1, func(i, j int) bool {
+		return a1[i] < a1[j]
+	})
+	sort.Slice(a2, func(i, j int) bool {
+		return a2[i] < a2[j]
+	})
+
+	return reflect.DeepEqual(a1, a2)
 }
 
 func main() {
-	ch1 := make(chan int)
+	// ch1 := make(chan int, 10)
+	// ch2 := make(chan int, 10)
 
-	t1 := tree.New(1)
-	go Walk(t1, ch1)
+	t1 := tree.New(2)
+	t2 := tree.New(1)
 
-	fmt.Println(t1)
+	fmt.Println(Same(t1, t2))
 
-	for i := range ch1 { 
-		fmt.Println(i)
-	}
+
 
 }
