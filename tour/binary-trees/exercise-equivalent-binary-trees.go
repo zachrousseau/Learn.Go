452d7a062c2c24744136a93ee26dfa92d442c039
zachrousseau <zachrousseau0@gmail.com>
2024-08-15 20:56:06 -0400

    Fix walk function

--- tour/binary-trees/exercise-equivalent-binary-trees.go
+++ tour/binary-trees/exercise-equivalent-binary-trees.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	"sort"
+	// "sort"
 
 	"golang.org/x/tour/tree"
 
-	"reflect"
+	// "reflect"
 )
 
 // Walk walks the tree t sending all values
@@ -23,53 +23,18 @@ func Walk(t *tree.Tree, ch chan int) {
 
 }
 
-// Same determines whether the trees
-// t1 and t2 contain the same values.
-func Same(t1, t2 *tree.Tree) bool {
-
-	ch1 := make(chan int, 10)
-	ch2 := make(chan int, 10)
-
-	go func() {
-		Walk(t1, ch1)
-		close(ch1)
-	}()
-
-	go func() {
-		Walk(t2, ch2)
-		close(ch2)
-	}()
-
-	var a1, a2 []int
-
-	for i := range ch1 { 
-		a1 = append(a1, i)
-	}
-	for i := range ch2 { 
-		a1 = append(a2, i)
-	}
-
-
-	// Sort the slices
-	sort.Ints(a1)
-	sort.Ints(a2)
-
-	fmt.Println(a1)
-	fmt.Println(a2)
-
-	// Compare the sorted slices
-	return reflect.DeepEqual(a1, a2)
-}
 
 func main() {
-	// ch1 := make(chan int, 10)
-	// ch2 := make(chan int, 10)
+	ch := make(chan int)
 
-	t1 := tree.New(1)
-	t2 := tree.New(1)
+	// Start the Walk function in a goroutine
+	go func() {
+		Walk(tree.New(1), ch)
+		close(ch) // Close the channel after all values have been sent
+	}()
 
-	fmt.Println(Same(t1, t2))
-
-
-
+	// Range over the channel to receive values
+	for node := range ch {
+		fmt.Println(node)
+	}
 }
