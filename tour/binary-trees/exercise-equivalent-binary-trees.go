94860249d78f8704f21864f60b04897ba7516e37
zachrousseau <zachrousseau0@gmail.com>
2024-08-15 20:38:33 -0400

    More work on the same function

--- tour/binary-trees/exercise-equivalent-binary-trees.go
+++ tour/binary-trees/exercise-equivalent-binary-trees.go
@@ -2,22 +2,25 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 	"sort"
 
 	"golang.org/x/tour/tree"
+
+	"reflect"
 )
 
 // Walk walks the tree t sending all values
 // from the tree to the channel ch.
 func Walk(t *tree.Tree, ch chan int) {
 
-	if t != nil {
-		ch <- t.Value
-		Walk(t.Left, ch)
-		Walk(t.Right, ch)
+	if t == nil {
+		return
 	}
-	
+
+	ch <- t.Value
+	Walk(t.Left, ch)
+	Walk(t.Right, ch)
+
 }
 
 // Same determines whether the trees
@@ -27,30 +30,34 @@ func Same(t1, t2 *tree.Tree) bool {
 	ch1 := make(chan int, 10)
 	ch2 := make(chan int, 10)
 
-	go Walk(t1, ch1)
-	go Walk(t2, ch2)
+	go func() {
+		Walk(t1, ch1)
+		close(ch1)
+	}()
 
-	close(ch1)
-	close(ch2)
+	go func() {
+		Walk(t2, ch2)
+		close(ch2)
+	}()
 
-	var a1 []int
-	var a2 []int
+	var a1, a2 []int
 
 	for i := range ch1 { 
 		a1 = append(a1, i)
 	}
-
 	for i := range ch2 { 
-		a2 = append(a2, i)
+		a1 = append(a2, i)
 	}
 
-	sort.Slice(a1, func(i, j int) bool {
-		return a1[i] < a1[j]
-	})
-	sort.Slice(a2, func(i, j int) bool {
-		return a2[i] < a2[j]
-	})
 
+	// Sort the slices
+	sort.Ints(a1)
+	sort.Ints(a2)
+
+	fmt.Println(a1)
+	fmt.Println(a2)
+
+	// Compare the sorted slices
 	return reflect.DeepEqual(a1, a2)
 }
 
@@ -58,7 +65,7 @@ func main() {
 	// ch1 := make(chan int, 10)
 	// ch2 := make(chan int, 10)
 
-	t1 := tree.New(2)
+	t1 := tree.New(1)
 	t2 := tree.New(1)
 
 	fmt.Println(Same(t1, t2))
