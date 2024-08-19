724b7ba771c1996929b4a9286853a97d71033a41
zachrousseau <zachrousseau0@gmail.com>
2024-08-18 20:16:03 -0400

    Make concurrent

--- tour/web-crawler/exercise-web-crawler.go
+++ tour/web-crawler/exercise-web-crawler.go
@@ -2,66 +2,75 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 type Fetcher interface {
-	// Fetch returns the body of URL and
-	// a slice of URLs found on that page.
 	Fetch(url string) (body string, urls []string, err error)
 }
 
-var fetched = make(map[string]bool)
+type UrlCache struct {
+	mu      sync.Mutex
+	fetched map[string]bool
+}
 
-// Crawl uses fetcher to recursively crawl
-// pages starting with url, to a maximum of depth.
-func Crawl(url string, depth int, fetcher Fetcher) {
-	// TODO: Fetch URLs in parallel.
-	// TODO: Don't fetch the same URL twice.
-	// This implementation doesn't do either:
+func (cache *UrlCache) Crawl(url string, depth int, fetcher Fetcher, wg *sync.WaitGroup) {
+	defer wg.Done()
 
 	if depth <= 0 {
 		return
 	}
-	body, urls, err := fetcher.Fetch(url)
 
+	cache.mu.Lock()
+	if cache.fetched[url] {
+		cache.mu.Unlock()
+		return
+	}
+	cache.fetched[url] = true
+	cache.mu.Unlock()
+
+	body, urls, err := fetcher.Fetch(url)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
 	fmt.Printf("found: %s %q\n", url, body)
-	fetched[url] = true
 
 	for _, u := range urls {
-
-		if (!fetched[u]) {
-			Crawl(u, depth-1, fetcher)
+		cache.mu.Lock()
+		if !cache.fetched[u] {
+			wg.Add(1)
+			go cache.Crawl(u, depth-1, fetcher, wg)
 		}
+		cache.mu.Unlock()
 	}
-	
-	return
 }
 
 func main() {
-	Crawl("https://golang.org/", 4, fetcher) // calls Crawl, uses fetcher as a populated fake fetcher 
+	cache := UrlCache{fetched: make(map[string]bool)}
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go cache.Crawl("https://golang.org/", 4, fetcher, &wg)
+	wg.Wait()
 }
 
-// fakeFetcher is Fetcher that returns canned results. It's a string map of the fakeResult struct which contains bodies and urls
-type fakeFetcher map[string]*fakeResult 
+// fakeFetcher is Fetcher that returns canned results.
+type fakeFetcher map[string]*fakeResult
 
 type fakeResult struct {
 	body string
 	urls []string
 }
 
-func (f fakeFetcher) Fetch(url string) (string, []string, error) { // this is a method for fakeFetcher 
+func (f fakeFetcher) Fetch(url string) (string, []string, error) {
 	if res, ok := f[url]; ok {
 		return res.body, res.urls, nil
 	}
 	return "", nil, fmt.Errorf("not found: %s", url)
 }
 
-// fetcher is a populated fakeFetcher.
 var fetcher = fakeFetcher{
 	"https://golang.org/": &fakeResult{
 		"The Go Programming Language",
